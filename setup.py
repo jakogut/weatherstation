4b00c7e8ba37752bb9d11722eb25fa5938e9832d
Joseph Kogut <joseph.kogut@gmail.com>
2016-09-28 14:38:52 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='weatherstation',
+    version='1.0',
+    author='Joseph Kogut',
+    author_email='joseph.kogut@gmail.com',
+    description='Personal Weather Station',
+    license='MIT',
+    packages=['weatherstation'],
+)
